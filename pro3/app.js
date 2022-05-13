64409d75e250b069daa7475488ad0d88c12ade6c
jamesjoel <jamesjoelj@gmail.com>
2022-05-13 16:43:23 +0530

    express day 1

---
+++ pro3/app.js
@@ -0,0 +1,5 @@
+var a = require("./hello");
+var b = a();
+var c = b.info();
+
+console.log(c);
---
+++ pro3/hello.js
@@ -0,0 +1,12 @@
+var test = function(){
+    return {
+        name : "rohit",
+        info : function(){
+            //console.log("***********");
+            return "TSS";
+        }
+    }
+}
+
+
+module.exports = test;
